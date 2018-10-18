0c54bf3c636d8e419f517287e0705da3e86ad997
Alexey Zapparov <ixti@member.fsf.org>
2018-10-18 21:18:41 +0000

    Do not requeue job that gone from inproc
    If job was gone from inproc then it could have happened becasue of:
    
    - it was already resurrected (worker failed to send hearbeat in time)
    - somebody remove that list, which is not what we supposed to handle

--- lib/sidekiq/ultimate/unit_of_work/requeue.lua
+++ lib/sidekiq/ultimate/unit_of_work/requeue.lua
@@ -1,3 +1,7 @@
-redis.call(ARGV[1], KEYS[1], ARGV[2])
-redis.call("LREM", KEYS[2], -1, ARGV[2])
-return 1
+if 1 == redis.call("LREM", KEYS[2], -1, ARGV[2]) then
+  redis.call(ARGV[1], KEYS[1], ARGV[2])
+  return 1
+end
+
+return 0
+
